312fc871cf16fdaaac22ca07a3d2a40dfd3d3867
HARIKRISHANA C <harikrishnanc0709@gmail.com>
2026-07-25 17:07:07 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+url = "https://books.toscrape.com/"
+response = requests.get(url)
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    books = []
+
+    for book in soup.find_all("article", class_="product_pod"):
+        title = book.h3.a["title"]
+        price = book.find("p", class_="price_color").text
+        availability = book.find("p", class_="instock availability").text.strip()
+
+        books.append({
+            "Title": title,
+            "Price": price,
+            "Availability": availability
+        })
+
+
+    df = pd.DataFrame(books)
+
+    print(df)
+
+
+    df.to_csv("books.csv", index=False)
+
+    print("\nDataset saved as books.csv")
+
+else:
+    print("Failed to retrieve webpage.")   
